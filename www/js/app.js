74e33c674d6c0331f4988bd5e10ccfeeab88d552
Ryan <ryan.shaw@min.vc>
2015-01-12 21:09:14 +0000

    Working with ionic

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('simplydo', ['ionic'])
+var simplydo = angular.module('simplydo', ['ionic']);
 
-.run(function($ionicPlatform) {
+simplydo.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +17,17 @@ angular.module('simplydo', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('app', {
+    url:'/app',
+    templateUrl: "templates/home.html"
+  });
+  $urlRouterProvider.otherwise('/app');
+
+});
+
+simplydo.controller('HomeCtrl', function($scope){
+  console.log('test');
+});
